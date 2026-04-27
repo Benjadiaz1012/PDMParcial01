3acebd9e3520110350c5b47ef71c1206cf94b9b4
Benjadiaz1012 <00301423@uca.edu.sv>
2026-04-27 15:55:40 -0600

    Add: Agregar proyecto basico

---
+++ app/src/main/java/com/example/parcial01pdm00301423/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.parcial01pdm00301423
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.parcial01pdm00301423.ui.theme.Parcial01PDM00301423Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Parcial01PDM00301423Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Parcial01PDM00301423Theme {
+        Greeting("Android")
+    }
+}
